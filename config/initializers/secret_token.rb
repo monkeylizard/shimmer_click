f36f7250b1670b3c3eb485aed47b8c8096f43e06
monkeylizard <Gazebo@amadeus-4.home>
2014-01-13 18:41:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShimmerClick::Application.config.secret_key_base = 'a9dc357a6dac2d259a2e27d7c2e5e39d8338d1d8fafa55d78f4c097b385289e690690808c0f89313d8e1ce471a0e183dfee0b84dc8f50b0c55cfe3597fdf84d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShimmerClick::Application.config.session_store :cookie_store, key: '_shimmer_click_session'
