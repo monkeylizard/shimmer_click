7bb3f7ec2e410849fef9340b10540ba12780750e
monkeylizard <Gazebo@amadeus-4.home>
2014-01-27 21:10:13 -0500

    Added user signup

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ ShimmerClick::Application.routes.draw do
 
   resources :quotes
 
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
